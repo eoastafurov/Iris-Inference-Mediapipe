25bb73c4b43d6eb9722b347dde910e5e9bf18d7c
Eugene Astafurov <eoastafurov1@gmail.com>
2021-10-26 15:32:58 +0300

    syntax

--- utils.py
+++ utils.py
@@ -4,7 +4,6 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import os
-import dlib
 
 mp_iris = mp.solutions.iris
 
@@ -36,7 +35,8 @@ class ColorUtils:
     def __init__(self):
         pass
 
-    def get_points(self, image: np.ndarray):
+    @staticmethod
+    def get_points(image: np.ndarray):
         with mp_iris.Iris() as iris:
             results = iris.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
         landmarks = np.array(results.face_landmarks_with_iris.ListFields()[0][1])
@@ -47,7 +47,8 @@ class ColorUtils:
 
         return points
 
-    def evaluate_iris_and_pupil_rad(self, iris: Iris) -> (int, int):
+    @staticmethod
+    def evaluate_iris_and_pupil_rad(iris: Iris) -> (int, int):
         r1 = math.sqrt((iris.center.x - iris.top.x)**2 + (iris.center.y - iris.top.y)**2)
         r2 = math.sqrt((iris.center.x - iris.bottom.x)**2 + (iris.center.y - iris.bottom.y)**2)
         r3 = math.sqrt((iris.center.x - iris.left.x)**2 + (iris.center.y - iris.left.y)**2)
@@ -74,22 +75,23 @@ class ColorUtils:
 
         return mask
 
-
-    def process_image(self, image: np.ndarray):
+    @staticmethod
+    def process_image(image: np.ndarray):
         """
         notes: image will be const
         """
-        points = self.get_points(image)
+        points = ColorUtils.get_points(image)
         left = Iris(points, align='left')
         right = Iris(points, align='right')
 
-        mask = np.array(self.make_mask(image, left) + self.make_mask(image, right), dtype=bool)
+        mask = np.array(ColorUtils.make_mask(image, left) + ColorUtils.make_mask(image, right), dtype=bool)
         b, g, r = cv2.split(image)
 
         return np.median(r[mask]), np.median(g[mask]), np.median(b[mask]), left, right
 
-    def plot_image_and_detected_color(self, image: np.ndarray, savepath: str):
-        r, g, b, left, right = self.process_image(image)
+    @staticmethod
+    def plot_image_and_detected_color(image: np.ndarray, savepath: str):
+        r, g, b, left, right = ColorUtils.process_image(image)
 
         fig, axs = plt.subplots(1, 3, figsize=(9, 4))
         axs[0].set_xticks([])
@@ -101,14 +103,13 @@ class ColorUtils:
 
         axs[0].imshow(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 
-        iris_radius, pupil_radius = self.evaluate_iris_and_pupil_rad(left)
+        iris_radius, pupil_radius = ColorUtils.evaluate_iris_and_pupil_rad(left)
         cv2.circle(image, (left.center.x, left.center.y), iris_radius, (0, 255, 0))
         cv2.circle(image, (left.center.x, left.center.y), pupil_radius, (0, 255, 0))
 
-        iris_radius, pupil_radius = self.evaluate_iris_and_pupil_rad(right)
+        iris_radius, pupil_radius = ColorUtils.evaluate_iris_and_pupil_rad(right)
         cv2.circle(image, (right.center.x, right.center.y), iris_radius, (0, 255, 0))
         cv2.circle(image, (right.center.x, right.center.y), pupil_radius, (0, 255, 0))
-
 
         axs[1].imshow(cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
         image[:, :, :] = [r, g, b]
@@ -134,31 +135,6 @@ def process_dir():
 
 def detect():
     process_dir()
-    # image = cv2.imread('1.png')
-    # cu = ColorUtils()
-    # cu.plot_image_and_detected_color(image, 'out.jpg')
-    # print(strange_cycle())
 
 
 detect()
-
-
-"""Some unused code"""
-def draw_circle(image: np.ndarray, point, rad=10):
-    cv2.circle(image, (point.x, point.y), rad, (0, 255, 0), int(rad))
-
-
-def draw_all_landmarks(image: np.ndarray, points: np.ndarray):
-    for point in points:
-        draw_circle(image, point, rad=2)
-
-
-def draw_only_iris(image: np.ndarray, points: np.ndarray):
-    left = Iris(points, align='left').get_list()
-    right = Iris(points, align='right').get_list()
-
-    for point in left:
-        draw_circle(image, point, rad=2)
-
-    for point in right:
-        draw_circle(image, point, rad=2)
